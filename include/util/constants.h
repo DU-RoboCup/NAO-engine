223c5217f5e57ef5d6accd0930ccd2bd7453a0a0
DavidMChan <david@iridescent.io>
2016-06-20 18:37:42 +0000

    Added a bunch of fun macros to common.h file

---
+++ include/util/constants.h
@@ -0,0 +1,34 @@
+/*
+Copyright (c) 2016 "University of Denver"
+
+This file is part of Pineapple.
+
+Pineapple is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION HISTORY
+-- Created by David Chan 6/20/16
+
+// FILE DESCRIPTION
+
+Define some useful floating point macros
+*/
+
+#ifndef _CONSTANTS_h_GUARD
+#define _CONSTANTS_h_GUARD
+
+#define EPSILON    0.0000001
+#define PI         3.1415926
+#define PHI        2.6180339
+
+#endif /*_COMMON_h_GUARD
