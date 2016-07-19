2dd0c869d6fc5027187474309ae4a2e51cb25748
David Chan <DavidMChan@users.noreply.github.com>
2016-07-19 13:57:04 -0600

    Fixed build scripts

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
  #!/bin/bash
- 
- # Build the main projects
- qibuild make -c linux64
- qibuild make -c cross-atom
- 
- # Copy the config folder
- cp -R /home/vagrant/NAO/NAO-engine/config /home/vagrant/NAO/NAO-engine/build-cross-atom/sdk/bin/
- cp -R /home/vagrant/NAO/NAO-engine/config /home/vagrant/NAO/NAO-engine/build-linux64/sdk/bin/
- 
- # Build the docs
- cd build-linux64
- echo "Building Documentation"
- make docs > /dev/null
- mv html/ docs/html
- mv latex/ docs/latex
- rm -rf ../docs/html
- rm -rf ../docs/latex
- mv docs/* ../docs/
+mkdir -p /home/vagrant/NAO/NAO-engine/build
+cd /home/vagrant/NAO/NAO-engine/build
+cmake ../ -DUSE_CROSS=NO
+make
+rm -rf *
+cmake ../ -DUSE_CROSS=YES
+make
+cd ..
